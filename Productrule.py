2d1b7b08faaa5ded64bf4f004dcb581285ad61e1
Guduly <133545858+Guduly@users.noreply.github.com>
2023-10-22 15:27:55 -0500

    Update Productrule.py
    Corrected the Formula

--- Productrule.py
+++ Productrule.py
@@ -2,10 +2,8 @@
 def productRule(base2, exponent2, exponent1, base1, var1):
     
     ## First step
-    print("Formula: func1 * 'func2 + 'func1 * func2/n")
+    print("Formula: 'func1 * func2 + func1 * 'func2")
 
-    return print(f"{(base1)(base2 * exponent2)}{var1}^{exponent1+(exponent2-1)} + {(base2)(base1 * exponent1)}{var1}^{exponent2+(exponent1-1)}")
-
+    return print(f"({(base1*exponent1)}{var1}^{exponent1-1})({base2}{var1}^{exponent2}) + ({(base2*exponent2)}{var1}^{exponent2-1})({base2}{var1}^{exponent2})")
+                 
 productRule(base2=2,exponent2=3,exponent1=2,base1=3,var1='x')
-
-
